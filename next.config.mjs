70b6f38d21125ef4624c37df02fb57427ea0eaa2
mkom <maskom@outlook.com>
2025-03-18 22:09:05 +0700

    remove https

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
-import fs from 'fs';
-import https from 'https';
+// import fs from 'fs';
+// import https from 'https';
 
 const nextConfig = {
   reactStrictMode: true,
-  devServer: {
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem'),
-    },
-  },
+  // devServer: {
+  //   https: {
+  //     key: fs.readFileSync('./localhost-key.pem'),
+  //     cert: fs.readFileSync('./localhost.pem'),
+  //   },
+  // },
 };
 
 export default nextConfig;
