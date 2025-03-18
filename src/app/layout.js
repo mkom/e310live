2154b4876058e8ba54671b574a1cb6370848dc4f
mkom <maskom@outlook.com>
2025-03-19 01:46:40 +0700

    bg body

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} antialiased`}
+        className={`${poppins.className} antialiased bg-white`}
       >
         <TopNavbar/>
         {children}
